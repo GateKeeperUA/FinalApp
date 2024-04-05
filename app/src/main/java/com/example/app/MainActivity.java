4cb3258eb6cd5307cfa1a5315a0379d0b5ffc85e
yopoitio <rafa.ba@sapo.pt>
2024-04-05 17:06:48 +0100

    Fingerprint authentication and NFC UID reading done

--- app/src/main/java/com/example/app/MainActivity.java
+++ app/src/main/java/com/example/app/MainActivity.java
@@ -1,23 +1,37 @@
 package com.example.app;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.hardware.fingerprint.FingerprintManager;
 import android.net.Uri;
+import android.nfc.NfcAdapter;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.biometric.BiometricManager;
+import androidx.biometric.BiometricPrompt;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationView;
 
+import java.util.Arrays;
+import java.util.concurrent.Executor;
 
-    public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
 
     public static String room;
@@ -25,11 +39,15 @@ import com.google.android.material.navigation.NavigationView;
     DrawerLayout drawerLayout;
     Toolbar toolbar;
     BottomNavigationView bottomNavigationView;
+    BiometricPrompt biometricPrompt;
+    BiometricPrompt.PromptInfo promptInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Biometric(true);
 
         toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -53,10 +71,9 @@ import com.google.android.material.navigation.NavigationView;
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                bottomNavigationView.getMenu().findItem(R.id.home)
-                        .setIcon(R.drawable.home_outlined);
-                bottomNavigationView.getMenu().findItem(R.id.shorts)
-                        .setIcon(R.drawable.lock_outlined);
+                bottomNavigationView.getMenu().findItem(R.id.home).setIcon(R.drawable.home_outlined);
+                bottomNavigationView.getMenu().findItem(R.id.shorts).setIcon(R.drawable.lock_outlined);
+                Log.d("Mudança","ola");
 
                 if (item.getItemId() == R.id.home) {
                     getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
@@ -71,8 +88,6 @@ import com.google.android.material.navigation.NavigationView;
                 return false;
             }
         });
-
-
     }
 
     @Override
@@ -106,8 +121,77 @@ import com.google.android.material.navigation.NavigationView;
 
         drawerLayout.closeDrawer(GravityCompat.START);
         return true;
-}
+    }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (getIntent().getAction().equals(NfcAdapter.ACTION_TAG_DISCOVERED)) {
+            SharedPreferences sharedPreferences = getSharedPreferences("NFC Tag", Context.MODE_PRIVATE);
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putString("key", ByteArrayToHexString(getIntent().getByteArrayExtra(NfcAdapter.EXTRA_ID)));
+            editor.apply();
 
+            bottomNavigationView.getMenu().findItem(R.id.home).setIcon(R.drawable.home_outlined);
+            bottomNavigationView.getMenu().findItem(R.id.shorts).setIcon(R.drawable.lock_outlined);
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new nfcFragment()).commit();
+            bottomNavigationView.getMenu().findItem(R.id.shorts).setIcon(R.drawable.nav_lock);
 
+            Toast.makeText(this,"NFC Tag saved",Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private String ByteArrayToHexString(byte [] inarray) {
+        int i, j, in;
+        String [] hex = {"0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"};
+        String out= "";
+
+        for(j = 0 ; j < inarray.length ; ++j)
+        {
+            in = (int) inarray[j] & 0xff;
+            i = (in >> 4) & 0x0f;
+            out += hex[i];
+            i = in & 0x0f;
+            out += hex[i];
+        }
+        return out;
+    }
+
+    private void Biometric(boolean able) {
+        if (able) {
+            BiometricManager biometricManager = BiometricManager.from(this);
+            switch (biometricManager.canAuthenticate()) {
+                case BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE:
+                    Toast.makeText(getApplicationContext(), "Device doesn't have fingerprint scanner", Toast.LENGTH_SHORT).show();
+                    break;
+                case BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE:
+                    Toast.makeText(getApplicationContext(), "Fingerprint scanner not working", Toast.LENGTH_SHORT).show();
+                    break;
+                case BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED:
+                    Toast.makeText(getApplicationContext(), "No fingerprint assigned", Toast.LENGTH_SHORT).show();
+                    break;
+            }
+            Executor executor = ContextCompat.getMainExecutor(this);
+            biometricPrompt = new BiometricPrompt(MainActivity.this, executor, new BiometricPrompt.AuthenticationCallback() {
+                @Override
+                public void onAuthenticationError(int errorCode, @NonNull CharSequence errString) {
+                    super.onAuthenticationError(errorCode, errString);
+                }
+
+                @Override
+                public void onAuthenticationSucceeded(@NonNull BiometricPrompt.AuthenticationResult result) {
+                    super.onAuthenticationSucceeded(result);
+                    drawerLayout.setVisibility(View.VISIBLE);
+                }
+
+                @Override
+                public void onAuthenticationFailed() {
+                    super.onAuthenticationFailed();
+                }
+            });
+
+            promptInfo = new BiometricPrompt.PromptInfo.Builder().setTitle("GateKeeper").setDeviceCredentialAllowed(true).build();
+            biometricPrompt.authenticate(promptInfo);
+        }
+    }
 }
--- app/src/main/java/com/example/app/nfcFragment.java
+++ app/src/main/java/com/example/app/nfcFragment.java
@@ -2,34 +2,28 @@ package com.example.app;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.nfc.NfcAdapter;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 
 public class nfcFragment extends Fragment {
-    ImageButton ImageButton;
-    public static final String TAG = "CardEmulationFragment";
 
     /** Called when sample is created. Displays generic UI with welcome text. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        //Code to store UID in the phone
-        /*SharedPreferences sharedPreferences = getActivity().getSharedPreferences("MyPrefs", Context.MODE_PRIVATE);
-        SharedPreferences.Editor editor = sharedPreferences.edit();
-        editor.putString("key", "...");
-        editor.apply();
+        SharedPreferences sharedPreferences = getActivity().getSharedPreferences("NFC Tag", Context.MODE_PRIVATE);
+        String UID = sharedPreferences.getString("key", "00000000");
+        Log.d("Shared_preference",UID);
 
-        //Code to read stored UID in the phone
-        SharedPreferences sharedPreferences = getActivity().getSharedPreferences("MyPrefs", Context.MODE_PRIVATE);
-        String UID = sharedPreferences.getString("key", "00 00 00 00");
-        Log.d("Shared_preference",UID);*/
     }
 
     @Override
@@ -40,6 +34,4 @@ public class nfcFragment extends Fragment {
 
         return v;
     }
-
-
 }
